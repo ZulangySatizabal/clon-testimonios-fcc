ffe67316ecde4febdc52d9f958310d4a2e784fb3
Zulangy Satizabal <zulangy.sati15@gmail.com>
2023-09-15 11:11:19 -0500

    usando props

--- src/components/Testimonio.js
+++ src/components/Testimonio.js
@@ -1,23 +1,22 @@
 import React from "react";
 import "../stylesheets/Testimonio.css";
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
       <img
         className="imagen-testimonio"
-        src={require("../img/test-Shawn.png")}
+        src={require(`../img/test-${props.imagen}.png`)}
         alt="Foto de Shawn"
       />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Shawn Wang en Singapur</p>
-        <p className="cargo-testimonio">Ingeniero de Software en Amazon</p>
-        <p className="texto-testimonio">
-          "Da miedo cambiar de carrera. Solo gané la confianza de que podía
-          programar trabajando a través de los cientos de horas de lecciones
-          gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis
-          cifras como ingeniero de software. freeCodeCamp cambió mi vida."
+        <p className="nombre-testimonio">
+          {props.nombre} en {props.pais}
         </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en {props.empresa}
+        </p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
     </div>
   );
